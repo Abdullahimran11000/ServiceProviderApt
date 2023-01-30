4469ce87fa79beb5b796d7fceee9b2a0d6578897
hamad4596 <181370131@gift.edu.pk>
2023-01-30 22:52:28 +0500

    30-Jan-2022
    Drawer Menu Update.

--- src/assets/styles/AnimatedDrawerStyle/DrawerStyle.js
+++ src/assets/styles/AnimatedDrawerStyle/DrawerStyle.js
@@ -4,11 +4,11 @@ import { AppColor } from '../../colors/AppColors';
 
 const DrawerStyle = StyleSheet.create({
     mainView: {flex: 1, backgroundColor: AppColor.primary},
-    baseViewProfilePic: {width: wp('16'), height: wp('16'), borderRadius: wp('8'), marginLeft: wp('5'), marginTop: wp('15')},
+    baseViewProfilePic: {width: wp('16'), height: wp('16'), borderRadius: wp('8'), marginLeft: wp('5'), marginTop: wp('5')},
     imageStyle: {width: wp('16'), height: wp('16'), borderRadius: wp('8')},
     nameText: {marginLeft: wp('5'), marginTop: wp('2'), fontFamily: 'Poppins-Bold', fontSize: wp('5'), color: AppColor.white},
     designationText: {marginLeft: wp('5'), fontFamily: 'Poppins-Light', fontSize: wp('3.5'), color: AppColor.white},
-    menuView: {marginTop: wp('18'), marginLeft: wp('5')},
+    menuView: {marginTop: wp('12'), marginLeft: wp('5')},
     menuView2: {marginTop: wp("-2"), marginLeft: wp('5')},
     menuItemView: {width: wp('60'), marginBottom: wp('8'), flexDirection: 'row'},
     menuItemText: {marginLeft: wp('3'), fontFamily: 'Poppins-Medium', fontSize: wp('3.7'), color: AppColor.white},
