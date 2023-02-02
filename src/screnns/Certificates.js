bb6e0139929aaf63460cfe42f9c0409d52584c0b
hamad4596 <181370131@gift.edu.pk>
2023-02-03 00:40:24 +0500

    2-Feb-2023
    Certificates screen updated.

--- src/screnns/Certificates.js
+++ src/screnns/Certificates.js
@@ -7,6 +7,7 @@ import {
   TouchableOpacity,
   FlatList,
   Image,
+  ImageBackground,
 } from 'react-native';
 import LinearGradient from 'react-native-linear-gradient';
 import {DashboardStyle} from '../assets/styles/DashboardStyle/DashboardStyle';
@@ -19,6 +20,7 @@ import {AppColor} from '../assets/colors/AppColors';
 import NeoButton from '../components/NeoMorphButton/NeoButton';
 import Feather from 'react-native-vector-icons/Feather';
 import Ionicons from 'react-native-vector-icons/Ionicons';
+import Foundation from 'react-native-vector-icons/Foundation';
 import {Neomorph} from 'react-native-neomorph-shadows';
 import {useNavigation} from '@react-navigation/core';
 import {launchImageLibrary} from 'react-native-image-picker';
@@ -31,38 +33,44 @@ const Certificates = () => {
   const [uploadImageListForZoom, setUploadImageListForZoom] = useState([]);
   const [selectedImageUri, setSelectedImageUri] = useState('');
 
-  const pressHandler = async () => {
-    let result = await launchImageLibrary({}, arr => {
-      setUploadImageList(oldImageList => [...oldImageList, {id : Math.random(), url: arr.assets[0].uri}]);
-      // setUploadImageListForZoom(oldImageList => [
-      //   ...oldImageList,
-      //   {url: arr.assets[0].uri},
-      // ]);
+  const imagePickerHandler = async () => {
+    await launchImageLibrary({}, arr => {
+      setUploadImageList(oldImageList => [
+        ...oldImageList,
+        {id: Math.random(), url: arr.assets[0].uri},
+      ]);
     });
   };
 
-  const arrayUpdation = (id, url) =>{
-    const filteredArray = uploadImageList.filter(item=>item.id !== id)
-    if(uploadImageListForZoom[0].url === url){
-      setUploadImageListForZoom([])
-      setSelectedImageUri('')
-    }//if
-    setUploadImageList([...filteredArray])
-  }
+  const flatListUpdation = (id, url) => {
+    const filteredArray = uploadImageList.filter(item => item.id !== id);
+    if (uploadImageListForZoom[0].url === url) {
+      setUploadImageListForZoom([]);
+      setSelectedImageUri('');
+    } //if
+    setUploadImageList([...filteredArray]);
+  };
 
   const renderImageList = ({item}) => {
     return (
       <TouchableOpacity
         onPress={() => {
-          setUploadImageListForZoom([])
+          setUploadImageListForZoom([]);
           setSelectedImageUri(setUploadImageListForZoom([{url: item.url}]));
-        }} onLongPress={()=>{arrayUpdation(item.id, item.url)}}>
-        <Neomorph
-          style={CertificatesStyle.neumorphListView}>
-          <Image
+        }}>
+        <Neomorph style={CertificatesStyle.neumorphListView}>
+          <ImageBackground
+            imageStyle={{borderRadius: wp('5')}}
             source={{uri: item.url}}
-            style={CertificatesStyle.listImageView}
-          />
+            style={CertificatesStyle.listImageView}>
+            <TouchableOpacity
+              style={CertificatesStyle.crossSign}
+              onPress={() => {
+                flatListUpdation(item.id, item.url);
+              }}>
+              <Foundation name="x" size={wp('4')} color={AppColor.whiteShade} />
+            </TouchableOpacity>
+          </ImageBackground>
         </Neomorph>
       </TouchableOpacity>
     );
@@ -79,20 +87,18 @@ const Certificates = () => {
                 navigation.goBack();
               }}>
               <Ionicons name="menu" color={AppColor.black} size={wp('7')} />
-              <Text style={[CertificatesStyle.textStyle, {width: wp('50'), position: 'absolute', left: wp('8')}]}>Go To Dashboard</Text>
+              <Text style={CertificatesStyle.textStyle1}>Go To Dashboard</Text>
             </TouchableOpacity>
             <TouchableOpacity style={CertificatesStyle.textCont}>
-            <Text style={[CertificatesStyle.textStyle, {width: wp('30')}]}>Certificates</Text>
+              <Text style={CertificatesStyle.textStyle}>Certificates</Text>
             </TouchableOpacity>
           </View>
         </View>
 
         <View style={CertificatesStyle.imageViewCard}>
-          <Neomorph
-            style={CertificatesStyle.imageNeumorphCard}>
+          <Neomorph style={CertificatesStyle.imageNeumorphCard}>
             {selectedImageUri === '' ? (
-              <View
-                style={CertificatesStyle.noImageView}>
+              <View style={CertificatesStyle.noImageView}>
                 <Feather
                   name="camera-off"
                   size={wp('50')}
@@ -106,7 +112,6 @@ const Certificates = () => {
                   style={{width: wp('90'), height: hp('60')}}
                 />
               </View>
-              
             )}
             {/* <View
               style={{
@@ -152,18 +157,14 @@ const Certificates = () => {
           showsHorizontalScrollIndicator={false}
         />
 
-        <View
-          style={CertificatesStyle.buttonView}>
-          <TouchableOpacity onPress={pressHandler}>
+        <View style={CertificatesStyle.buttonView}>
+          <TouchableOpacity onPress={imagePickerHandler}>
             <NeoButton
               width={wp('40')}
               height={hp('8')}
               backgroundColor={AppColor.primary}
               borderRadius={wp('7')}>
-              <Text
-                style={CertificatesStyle.buttonText}>
-                UPLOAD
-              </Text>
+              <Text style={CertificatesStyle.buttonText}>UPLOAD</Text>
             </NeoButton>
           </TouchableOpacity>
         </View>
