71810a5227112b7031ce1115217097d0d8d48367
hamad4596 <181370131@gift.edu.pk>
2023-03-07 16:24:34 +0500

    07-Mar-2023
    Updates

--- src/assets/styles/TryAnotherWayStyle.js
+++ src/assets/styles/TryAnotherWayStyle.js
@@ -4,8 +4,10 @@ import { AppColor } from "../colors/AppColors";
 
 const TryAnotherWayStyle = StyleSheet.create({
     viewOne: {alignItems: 'center', marginTop: hp('10')},
-    neomorphOne: {width: wp('50'),height: hp('25'),borderRadius: wp('30'),backgroundColor: AppColor.whiteShade,shadowRadius: 4},
-    lottyStyle: {alignSelf: 'center',height: hp('38'), bottom: hp('3'), left: wp(0.5)},
+    neomorphOne: {width: wp('50'),height: wp('50'),borderRadius: wp('50'),backgroundColor: AppColor.railBorderColor,shadowRadius: 4, alignItems: 'center', justifyContent: 'center'},
+    neomorphThree: {width: wp('42'),height: wp('42'),borderRadius: wp('42'),backgroundColor: AppColor.whiteShade,shadowRadius: 0, alignItems: 'center', justifyContent: 'center'},
+    neomorphFour: {width: wp('36'),height: wp('36'),borderRadius: wp('36'),backgroundColor: AppColor.whiteShade,shadowRadius: 4, alignItems: 'center', justifyContent: 'center'},
+    lottyStyle: {width: wp('28'), height: wp('28'), top: wp('-2')},
     viewTwo:{width: wp('90'), alignSelf: 'center', marginTop: hp(5)},
     text:{ fontFamily: 'Poppins-SemiBold',fontSize: wp(4.5),color: AppColor.black},
     viewThree:{alignSelf: 'center'},
